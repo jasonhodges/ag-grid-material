582e3e6b4bed64b6c9009fbd901e4990a90be202
Petyo Ivanov <underlog@gmail.com>
2017-08-22 19:00:37 +0300

    ag-grid and material components side by side

--- angular-material/src/app/app.component.ts
+++ angular-material/src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { GridOptions } from "ag-grid/main";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+    title = 'app';
+
+    private gridOptions:GridOptions = <GridOptions>{};
+    public rowData:any[];
+    private columnDefs:any[];
+
+
+    constructor() {
+        this.columnDefs = [
+            {headerName: "Make", field: "make"},
+            {headerName: "Model", field: "model"},
+            {headerName: "Price", field: "price"}
+        ];
+
+        this.rowData = [
+            {make: "Toyota", model: "Celica", price: 35000},
+            {make: "Ford", model: "Mondeo", price: 32000},
+            {make: "Porsche", model: "Boxter", price: 72000}
+        ]
+    }
 }
